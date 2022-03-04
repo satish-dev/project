ff0e722d37d256a52e00a51656aa34016916a0bd
Satish kumar <satish.kumar@kelltontech.com>
2022-03-04 18:43:53 +0530

    Introduced Multiplication

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
         {
             return $this->num1+ $this->num2;
         }
+        public function multiplication()
+        {
+            return $this->num1+ $this->num2;
+        }
     }
     $obj = new A();
     echo "Sum of $obj->num1 and $obj->num2 is ". $obj->sum()."<br/>";
+    echo "Multiplication of $obj->num1 and $obj->num2 is ". $obj->mulitplication2()."<br/>";
 ?>
